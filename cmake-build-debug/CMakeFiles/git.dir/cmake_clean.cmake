03af83ea0996df7a7a02f340a10df72d39394bd3
Grzegorz Kaczmarek <gkaczmarek3@icloud.com>
2021-01-27 16:35:49 +0100

    test

---
+++ cmake-build-debug/CMakeFiles/git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git.dir/main.cpp.obj"
+  "git.exe"
+  "git.exe.manifest"
+  "git.pdb"
+  "libgit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
